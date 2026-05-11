1e65a381c7c9a13f47717abf3ca6eb2af8791e7d
heathcetide <2148582258@qq.com>
2026-05-11 11:18:01 +0800

    feat: del rtcsfu module for this project

--- pkg/rtcsfu/defaults.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2026 LingByte. All rights reserved.
-// SPDX-License-Identifier: AGPL-3.0
-
-package rtcsfu
-
-// Defaults for WebRTC voice interoperability with pkg/media Opus paths (48 kHz clock).
-const (
-	OpusClockHz = 48000
-)
