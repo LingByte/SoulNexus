37a6f509bdf28ef73e7040095b22597c8cd1b25e
heathcetide <2148582258@qq.com>
2026-06-04 14:37:33 +0800

    Revert "refactor: unify main content area styling in Layout"
    This reverts commit 85c885a58404d306e4acc09aca1dc763eb5d0f27.

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -10,13 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
         <div className="h-screen flex flex-row bg-background text-foreground">
             <Sidebar />
             <main className="flex-1 min-h-0 overflow-auto bg-background lg:ml-0 pt-[104px] lg:pt-0">
-                <div className="h-full flex flex-col">
-                    <div className="flex-1 overflow-auto">
-                        <div className="mx-auto w-full max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                            {children}
-                        </div>
-                    </div>
-                </div>
+                {children}
             </main>
         </div>
     )
