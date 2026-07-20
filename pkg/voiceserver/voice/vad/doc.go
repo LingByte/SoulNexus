120e82ff8e14b438048782362cc98c429349bb71
heathcetide <2148582258@qq.com>
2026-07-20 15:15:22 +0800

    refactor(pkg): update voice server stack

--- pkg/voiceserver/voice/vad/doc.go
+++
@@ -1,6 +0,0 @@
-// Package vad provides barge-in detection for voice-server transports.
-//
-// The implementation delegates to github.com/LingByte/lingllm/vad (RMS energy
-// detector). Remote HTTP/WebSocket VAD services are also available from the
-// same lingllm module when needed.
-package vad
